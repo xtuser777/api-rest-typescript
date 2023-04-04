01165b96a7f86e8ddbd2fb9b79d98a9eeef1eef8
Lucas Oliveira <lucaoxt@gmail.com>
2023-04-03 21:01:48 -0300

    03/04/2023-21:01

--- src/controller/freight-order-controller.ts
+++ src/controller/freight-order-controller.ts
@@ -28,6 +28,11 @@ import { FreightOrderItem } from '../model/freight-order-item';
 import { LoadStep } from '../model/load-step';
 import { OrderStatus } from '../model/order-status';
 import { ActiveUser } from '../util/active-user';
+import { IndividualPerson } from '../model/individual-person';
+import { BillPay } from '../model/bill-pay';
+import { EnterprisePerson } from '../model/enterprise-person';
+import { ReceiveBill } from '../model/receive-bill';
+import { Event } from '../model/event';
 
 export class FreightOrderController {
   responseBuild = async (order: FreightOrder): Promise<any> => {
@@ -216,6 +221,49 @@ export class FreightOrderController {
       if (responseStatus == -1)
         return res.status(400).json('erro conexao banco de dados');
     }
+    const responseProprietaryBill = await this.releaseProprietaryBill(
+      response,
+      order.driver,
+      order.paymentFormDriver,
+      order.amountDriver,
+      order.amountDriverEntry,
+    );
+    if (responseProprietaryBill < 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (responseProprietaryBill == -10)
+        return res.status(400).json('erro ao inserir a conta do proprietátio');
+      if (responseProprietaryBill == -5)
+        return res.status(400).json('compos incorretos na conta do proprietátio');
+      if (responseProprietaryBill == -1)
+        return res.status(400).json('erro conexao banco de dados');
+    }
+    const responseOrderBill = await this.releaseOrderBill(
+      response,
+      order.budget,
+      order.sale,
+      order.representation,
+      order.paymentFormFreight,
+      order.value,
+    );
+    if (responseOrderBill < 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (responseOrderBill == -10)
+        return res.status(400).json('erro ao inserir a conta do pedido');
+      if (responseOrderBill == -5)
+        return res.status(400).json('compos incorretos na conta do pedido');
+      if (responseOrderBill == -1)
+        return res.status(400).json('erro conexao banco de dados');
+    }
+    const responseEvent = await this.createEvent(response, order.description, 1);
+    if (responseEvent < 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (responseEvent == -10) return res.status(400).json('erro ao inserir o evento');
+      if (responseEvent == -5) return res.status(400).json('compos incorretos no evento');
+      if (responseEvent == -1) return res.status(400).json('erro conexao banco de dados');
+    }
     await Database.instance.commit();
     await Database.instance.close();
 
@@ -251,7 +299,311 @@ export class FreightOrderController {
     ).save(order);
   };
 
+  releaseProprietaryBill = async (
+    order: number,
+    driver: number,
+    form: number,
+    amount: number,
+    adiantament: number,
+  ): Promise<number> => {
+    if (order <= 0 || driver <= 0 || amount <= 0) return -5;
+
+    let situation = 1;
+    let pendente = 0.0;
+
+    if (adiantament > 0) {
+      pendente = amount - adiantament;
+      situation = 2;
+    }
+
+    const d = (await new Driver().findOne(driver)) as Driver;
+    const p = (
+      await new IndividualPerson().find({ id: d.getPersonId() })
+    )[0] as IndividualPerson;
+
+    const drv = p.getName();
+
+    const activeUser = ActiveUser.getInstance() as ActiveUser;
+
+    const bill = await new BillPay(
+      0,
+      await new BillPay().getNewBill(),
+      new Date(),
+      1,
+      `Pagamento ao motorista ${drv}`,
+      drv,
+      1,
+      amount,
+      false,
+      1,
+      new Date(new Date().setMonth(new Date().getMonth() + 2)),
+      undefined,
+      0.0,
+      0,
+      0,
+      driver,
+      0,
+      249,
+      order,
+      0,
+      activeUser.getId(),
+    );
+
+    let response = await bill.save();
+
+    if (response < 0) return response;
+
+    if (pendente > 0) {
+      response = await new BillPay(
+        0,
+        await new BillPay().getNewBill(),
+        new Date(),
+        1,
+        `Pagamento ao motorista ${drv} (Pendência).`,
+        drv,
+        1,
+        pendente,
+        false,
+        1,
+        new Date(new Date().setMonth(new Date().getMonth() + 2)),
+        undefined,
+        0.0,
+        0,
+        0,
+        driver,
+        0,
+        249,
+        order,
+        0,
+        activeUser.getId(),
+      ).save();
+
+      if (response < 0) return response;
+
+      response = await bill.payOff(
+        form,
+        adiantament,
+        new Date().toISOString().substring(0, 10),
+        situation,
+        response,
+      );
+    }
+
+    return response;
+  };
+
+  releaseOrderBill = async (
+    order: number,
+    budget: number,
+    sale: number,
+    representation: number,
+    form: number,
+    amount: number,
+  ): Promise<number> => {
+    if (order <= 0 || form <= 0 || amount <= 0) return -5;
+    let payer = '';
+    if (budget > 0) {
+      const b = await new FreightBudget().findOne(budget);
+      payer = !b ? '' : b.getDescription();
+    } else if (sale > 0) {
+      const s = await new SalesOrder().findOne(sale);
+      payer = !s ? '' : s.getDescription();
+    } else if (representation > 0) {
+      const r = (await new Representation().findOne(representation)) as Representation;
+      const p = (
+        await new EnterprisePerson().find({ id: r.getPersonId() })
+      )[0] as EnterprisePerson;
+      payer = p.getFantasyName();
+    }
+    const activeUser = ActiveUser.getInstance() as ActiveUser;
+    const bill = new ReceiveBill(
+      0,
+      new Date(),
+      await new ReceiveBill().getNewBill(),
+      `Recebimento pedido: ${order}`,
+      payer,
+      amount,
+      false,
+      1,
+      new Date(new Date().setMonth(new Date().getDay() + 2)),
+      undefined,
+      0.0,
+      0,
+      0,
+      budget > 0 ? representation : 0,
+      0,
+      order,
+      activeUser.getId(),
+    );
+
+    const response = await bill.save();
+    if (response <= 0) return response;
+
+    const responseReceive = bill.receive(
+      form,
+      amount,
+      new Date().toISOString().substring(0, 10),
+      3,
+      0,
+    );
+
+    return response;
+  };
+
   delete = async (req: Request, res: Response): Promise<Response> => {
+    if (!req.params.id) return res.status(400).json('parametro ausente');
+    let id = 0;
+    try {
+      id = Number.parseInt(req.params.id);
+    } catch {
+      return res.status(400).json('parametro invalido');
+    }
+    await Database.instance.open();
+    const order = await new FreightOrder().findOne(id);
+    if (!order) {
+      await Database.instance.close();
+      return res.status(400).json('pedido ausente');
+    }
+    const activeUser = ActiveUser.getInstance() as ActiveUser;
+    await Database.instance.beginTransaction();
+    if (!(await this.deleteSteps(id)))
+      return res.status(400).json('Erro ao excluir as etapas de carregamento do pedido.');
+    if (!(await this.deleteItems(id)))
+      return res.status(400).json('Erro ao excluir os itens do pedido.');
+    if (!(await this.deleteStatus(id)))
+      return res.status(400).json('Erro ao excluir os status do frete.');
+    if (!(await this.deleteOrderBill(id)))
+      return res.status(400).json('Erro ao excluir a conta a receber do pedido.');
+    const responseProprietaryBill = await this.deleteProprietaryBill(id);
+    if (responseProprietaryBill <= 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (responseProprietaryBill == -15)
+        return res
+          .status(400)
+          .json(
+            'A conta a pagar do proprietário está paga ou parcialmente paga, estorne esta antes de excluir.',
+          );
+      if (responseProprietaryBill == -10)
+        return res
+          .status(400)
+          .json('Ocorreu um problema ao excluir a conta a pagar do proprietário.');
+      if (responseProprietaryBill == -5)
+        return res.status(400).json('Parametros invalidos.');
+      if (responseProprietaryBill == -1)
+        return res.status(400).json('Erro ao conectar ao banco de dados');
+    }
+    const response = await order.delete();
+    if (response <= 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (response == -10) return res.status(400).json('erro ao remover o pedido');
+      if (response == -5) return res.status(400).json('compos incorretos no pedido');
+      if (response == -1) return res.status(400).json('erro conexao banco de dados');
+    }
+    const responseEvent = await this.createEvent(id, order.getDescription(), 2);
+    if (responseEvent <= 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      if (responseEvent == -10) return res.status(400).json('erro ao criar o evento');
+      if (responseEvent == -5) return res.status(400).json('compos incorretos no evento');
+      if (responseEvent == -1) return res.status(400).json('erro conexao banco de dados');
+    }
+    await Database.instance.commit();
+    await Database.instance.close();
+
     return res.json('');
+  };
+
+  deleteItems = async (order: number): Promise<boolean> => {
+    if (order <= 0) return false;
+    const items = await new FreightOrderItem().find({ order });
+    if (items.length <= 0) return true;
+    for (const item of items) {
+      const r = await item.delete(order, item.getProductId());
+      if (r < 0) {
+        await Database.instance.rollback();
+        await Database.instance.close();
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  deleteSteps = async (order: number): Promise<boolean> => {
+    if (order <= 0) return false;
+    const steps = await new LoadStep().find({ order });
+    if (steps.length <= 0) return true;
+    for (const step of steps) {
+      const r = await step.delete();
+      if (r < 0) {
+        await Database.instance.rollback();
+        await Database.instance.close();
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  deleteStatus = async (order: number): Promise<boolean> => {
+    if (order <= 0) return false;
+    const statuses = await new OrderStatus().find({ order });
+    if (statuses.length == 0) return true;
+    for (const status of statuses) {
+      const r = await status.delete(order);
+      if (r < 0) {
+        await Database.instance.rollback();
+        await Database.instance.close();
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  deleteProprietaryBill = async (order: number): Promise<number> => {
+    if (order <= 0) return -5;
+    const bill = (await new BillPay().find({ freight: order }))[0];
+    if (!bill) return -10;
+    if (bill.getSituation() > 1) return -15;
+
+    return await bill.delete();
+  };
+
+  deleteOrderBill = async (order: number): Promise<boolean> => {
+    if (order <= 0) return false;
+    const bill = (await new BillPay().find({ freight: order }))[0];
+    if (!bill) return true;
+    const r = await bill.delete();
+    if (r < 0) {
+      await Database.instance.rollback();
+      await Database.instance.close();
+      return false;
+    }
+
+    return true;
+  };
+
+  createEvent = async (
+    order: number,
+    orderDescription: string,
+    method: number,
+  ): Promise<number> => {
+    if (order <= 0) return -5;
+    const activeUser = ActiveUser.getInstance() as ActiveUser;
+
+    return await new Event(
+      0,
+      method == 1
+        ? `Abertura do pedido de frete ${order}: ${orderDescription}`
+        : `Exclusão do pedido de frete ${order}: ${orderDescription}`,
+      new Date(),
+      new Date(),
+      0,
+      order,
+      activeUser.getId(),
+    ).save();
   };
 }
